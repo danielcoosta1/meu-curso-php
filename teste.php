efca48d9ec3036ba34965f63b3d5bc1879823408
Daniel Costa <daniel.dc697@gmail.com>
2025-11-17 14:10:37 -0300

    formatação do codigo

--- teste.php
+++ teste.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resposta do formulário</title>
 </head>
+
 <body>
     <h1>Dados Recebidos</h1>
     <?php
@@ -17,4 +19,5 @@
 
     <a href="exemplo1.php">Voltar ao formulário</a>
 </body>
+
 </html>
