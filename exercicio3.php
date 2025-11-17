778c6a6991c07ade982c12ee137f28f30874949f
Daniel Costa <daniel.dc697@gmail.com>
2025-11-17 17:05:20 -0300

    Array associativo

---
+++ exercicio3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays - Exercicio 1</title>
+</head>
+<body>
+    <h1>Lista de estados</h1>
+    <?php
+    $estados = ['Paraná', 'São Paulo', 'Rio de Janeiro', 'Minas Gerais', 'Bahia'];
+    echo "<p>O estado pedido é:" .$estados[3]. "</p>";
+    ?>
+</body>
+</html>
