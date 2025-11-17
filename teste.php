e387b50c40783aa1e81dc4ea245b37ec03ef202a
Daniel Costa <daniel.dc697@gmail.com>
2025-11-17 14:09:07 -0300

    Arquivo ao qual irá tratar os dados do formulário do exemplo1.php

---
+++ teste.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resposta do formulário</title>
+</head>
+<body>
+    <h1>Dados Recebidos</h1>
+    <?php
+    $nome = $_POST['nome'];
+    $idade = $_POST['idade'];
+    echo "Olá, $nome! Você tem $idade anos.";
+    ?>
+
+    <br>
+
+    <a href="exemplo1.php">Voltar ao formulário</a>
+</body>
+</html>
